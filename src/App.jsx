2a808aad3b43f840d727598755031641aff93137
Vincent <Vsuen123@gmail.com>
2022-11-03 14:24:38 -0400

    frame

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import { motion } from "framer-motion"
+import { useState } from "react"
 
 const App = () => {
-  return (
+  const [isOpen, setIsOpen] = useState(false)
+  
+return (
     <div className="parent-div">
-      <motion.div className="container" 
-      whileHover={{scale: 1.2}}
-      whileTap={{ scale: 1.1 }}
-      drag dragConstraints={{left: -100, right: 100, top: -100, bottom:100}}/>
+      <motion.div className="container"
+       whileHover={{scale: 1.2}} 
+       whileTap={{scale: 1.1}}>
+      
+      </motion.div>
     </div>
   )
 }
 
 export default App
+
+//drag dragConstraints={{left: -100, right: 100, top: -100, bottom:100}}
